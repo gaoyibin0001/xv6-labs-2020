01094819b4cf26487b63d142ab4f09f656811e76
GaoYibin <GAOY2@legal.regn.net>
2022-04-28 13:20:56 +0800

    step 1

--- kernel/vm.c
+++ kernel/vm.c
@@ -319,12 +319,15 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
     if((*pte & PTE_V) == 0)
       panic("uvmcopy: page not present");
     pa = PTE2PA(*pte);
-    flags = PTE_FLAGS(*pte);
-    if((mem = kalloc()) == 0)
-      goto err;
-    memmove(mem, (char*)pa, PGSIZE);
-    if(mappages(new, i, PGSIZE, (uint64)mem, flags) != 0){
-      kfree(mem);
+    *pte = *pte & ~PTE_W | PTE_COW; // old process flags no write and cow, so will new process
+    // flags = PTE_FLAGS(*pte) & ~PTE_W; // new process flags no write
+    flags = PTE_FLAGS(*pte); // new process flags no write
+
+    // if((mem = kalloc()) == 0)
+    //   goto err;
+    // memmove(mem, (char*)pa, PGSIZE);
+    if(mappages(new, i, PGSIZE, (uint64)pa, flags) != 0){
+      // kfree(mem);
       goto err;
     }
   }
